a016bf224e0de236dad161c8695096786f8daf09
jsanti123 <s.posada3@utp.edu.co>
2024-10-05 23:56:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ formularioUser.addEventListener('submit', async function(event) {
         })
     })
     const mensaje = await response.json();
+    console.log(mensaje);
     if (mensaje.message != "Guardado") {
         alert("Error al guardar la información");
     } else {
